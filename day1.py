9065bfb7f3d8984cf0c30a726db9305ee4b34203
rishiv1000 <rishiv1000@outlook.com>
2024-02-16 22:39:49 +0530

    its Day1

---
+++ day1.py
@@ -0,0 +1,18 @@
+# print("HELLOW\nWORLD")
+
+# print("Hwllo" + " WORLD")
+# print("hello" + "" + "world")
+#a= input("TYPE YOUT INPUT\n")
+#print(a)
+
+# print("YOUR NAME IS " + input("name"))
+
+# a=input("name")
+# print(len(a))
+
+# a= input("a:")
+# b= input("b:")
+# c=a
+# a=b
+# b=c
+# print(a,b)
