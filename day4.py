38ddf84a131198ca7dc392fd6306fd37f2842e54
rishiv1000 <rishiv1000@outlook.com>
2024-02-19 22:44:08 +0530

    day4

---
+++ day4.py
@@ -0,0 +1,110 @@
+# import random
+
+# inte = random.randint(1,10)
+# # print(inte)
+
+# # f =random.random()*5
+# # print(f)
+
+
+# match inte :
+#     case 2:
+#         print("this is 2")
+#     case 3:
+#         print("this is 3")    
+
+# import random
+
+# my_friends = input("Give me friends name by comma\n")
+
+# friends = my_friends.split(',')
+
+
+# a = random.choice(friends)
+
+# print(a)
+# fri_len = len(friends)
+
+# ranint = random.randint(0,fri_len -1 )
+
+
+# a = friends.random()
+
+# print(f"{friends[ranint]} is a my friends")
+
+
+# lista = ["*","*","*","*","*"]
+# listb = ["*","*","*","*","*"]
+# listc = ["*","*","*","*","*"]
+# listd = ["*","*","*","*","*"]
+
+# liste = [lista,listb,listc,listd]
+
+# print(f"{lista}\n{listb}\n{listc}\n{listd}")
+
+
+# col = int(input("enter col no.\n"))
+# row = int(input("enter row no.\n"))
+
+# liste[col][row] = "A"
+
+# print(f"{lista}\n{listb}\n{listc}\n{listd}")
+
+  
+# rock = '''
+
+# ---,_____________/
+#                 /
+#                / 
+#               / 
+# ---,_________/  
+# '''              
+
+# paper = '''
+
+# ---,_____________/
+#                 /
+#   ----------------/ 
+#               / 
+# ---,_________/  
+# '''              
+
+# scissors = '''
+
+# ---,_____________/
+#                 /
+#               ---------------- / 
+#              ---------------- / 
+# ---,_________/  
+# '''            
+
+
+# user = int(input("Type 0 for Rock , 1 for Paper , 2 for Scissors\n"))
+
+# IMG = [rock,paper,scissors]
+
+# print(IMG[user])
+# import random
+ 
+# num = random.randint(0,2)
+
+# print(f"Computer choose:") 
+# print(IMG[num])
+ 
+
+# if user == 0 and num == 1:
+#     print("LOSS")
+# elif user == 0 and num == 2:
+#     print("WIN")
+# elif user == 1 and num == 0:
+#     print("WIN")   
+# elif user == 1 and num == 2:
+#     print("LOSS")   
+# elif user == 2 and num == 0:
+#     print("LOSS")    
+# elif user == 2 and num == 1:
+#     print("WIN")              
+# elif user == num:
+#     print("DRAW")
+    
+
