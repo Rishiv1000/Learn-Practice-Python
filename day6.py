166ef7aae850d76f8a47ceddc8f6d0539d7f9b4b
rishiv1000 <rishiv1000@outlook.com>
2024-02-22 22:24:39 +0530

    day6 python practice

---
+++ day6.py
@@ -0,0 +1,22 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def moving():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+        
+
+for i in range (0,7):
+    moving()
+    
+while at_goal() != True:
+    moving()
+              
